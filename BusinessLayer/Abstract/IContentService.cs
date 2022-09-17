fb9e3d4169a1890478918d755034ff53ee93e071
FikretErduran <fikret.erd@gmail.com>
2022-09-17 13:10:44 +0200

    MvcProjeKampi

---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public  interface IContentService
+    {
+        List<Content> List();
+        List<Content> GetListByHeadingID(int id);
+        void ContentInsert(Content content);   
+        void ContentUpdate(Content content);   
+        void ContentDelete(Content content);
+        Content GetByID(int id);
+
+
+    }
+}
---
+++ BusinessLayer/Concrete/ContentManager.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContentManager : IContentService
+    {
+
+        IContentDal _contentDal;
+
+        public ContentManager(IContentDal contentDal)
+        {
+            _contentDal = contentDal;
+        }
+
+        public void ContentDelete(Content content)
+        {
+            _contentDal.Delete(content);
+        }
+
+        public void ContentInsert(Content content)
+        {
+            _contentDal.Insert(content);
+        }
+
+        public void ContentUpdate(Content content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Content GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Content> GetListByHeadingID(int id)
+        {
+            return _contentDal.List(x => x.HeadingID == id);
+        }
+
+        //parametreye baglı olarak Listeleme yapmak için gerekli
+        //şartı çagırmalıyız
+        public List<Content> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
